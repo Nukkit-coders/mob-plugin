902852397295bd5c3aab03b4695c0c2e0d0cadd3
PikyXoS <jarda.pikart1999@gmail.com>
2018-03-09 05:10:53 +0100

    Create DespawnTask.java

---
+++ src/main/java/de/kniffo80/mobplugin/DespawnTask.java
@@ -0,0 +1,34 @@
+package de.kniffo80.mobplugin;
+
+import cn.nukkit.entity.Entity;
+import cn.nukkit.level.Level;
+import cn.nukkit.scheduler.PluginTask;
+import de.kniffo80.mobplugin.entities.BaseEntity;
+
+/**
+ * @author PikyCZ
+ */
+public class DespawnTask extends PluginTask<MobPlugin> {
+
+    public MobPlugin plugin;
+
+    public DespawnTask(MobPlugin owner) {
+        super(owner);
+        this.plugin = owner;
+    }
+
+    @Override
+    public void onRun(int i) {
+        int count = 0;
+        for (Level level : this.plugin.getServer().getLevels().values()) {
+            for (Entity entity : level.getEntities()) {
+                if (entity instanceof BaseEntity) {
+                    entity.close();
+                    count++;
+                }
+            }
+        }
+        this.plugin.getServer().broadcastMessage(plugin.PluginPrefix + " Removed " + count + " entities from all levels.");
+    }
+
+}
